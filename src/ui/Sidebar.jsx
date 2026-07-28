98462425d9599fb5c2eb6e3d7c073f12f529b584
mahmoudEbeid2 <mahmoud.ebeid2002@gmail.com>
2026-07-28 21:25:35 +0300

    chore: rename Mostanad to Dossira in UI

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -84,7 +84,7 @@ export default function Sidebar() {
         <div className="w-8 h-8 rounded-lg bg-blue-600 flex items-center justify-center shadow-md shadow-blue-500/20">
           <FileCheck className="w-5 h-5 text-white" />
         </div>
-        <span className="text-xl font-bold text-gray-900 tracking-tight">Mostanad</span>
+        <span className="text-xl font-bold text-gray-900 tracking-tight">Dossira</span>
       </div>
 
       {/* Navigation */}
